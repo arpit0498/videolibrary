62cfe8655dd5e9b70072e09cc699475fb7d3ef43
arpit gupta <mycontact.arpit@gmail.com>
2023-07-29 20:17:30 +0530

    refactor for deployment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { LeftSidebar } from "./component/LeftSidebar/LeftSidebar";
 import { Homepage } from "./pages/Homepage/Homepage";
 import { VideoListing } from "./pages/VideoListing/VideoListing";
-import { WatchLater } from "./pages/WatchLater/WatchLater";
+// import { WatchLater } from "./pages/WatchLater/WatchLater";
 import { VideoDetail } from "./pages/VideoDetail/VideoDetail";
 import { Explore } from "./pages/Explore/Explore";
 import { Playlists } from "./pages/Playlists/Playlists";
 import { PlaylistMusicVideos } from "./pages/PlaylistMusicVideos/PlaylistMusicVideos";
+import { WatchLater } from "./pages/WatchLater/WatchLater";
 
 
 function App() {
