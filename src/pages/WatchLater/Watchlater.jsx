80013e104499b52cd1f6a94a8293bf5a9370d5c8
arpit gupta <32380690+arpit0498@users.noreply.github.com>
2023-08-11 21:23:05 +0530

    Update and rename watchlater.layout.css to Watchlater.jsx

---
+++ src/pages/WatchLater/Watchlater.jsx
@@ -0,0 +1,29 @@
+// .watchlater-container {
+//   margin-top: 2rem;
+// }
+
+// .watchlater-container .video-container {
+//   display: flex;
+//   gap: 3rem;
+//   flex-wrap: wrap;
+//   margin-top: 2rem;
+// }
+import { useDataContext } from "../../context/data/DataContext";
+
+// import "./watchlater.layout.css";
+import { VideoCard } from "../../component/VideoCard/VideoCard";
+
+export const WatchLater = () => {
+  const { watchLaterList } = useDataContext();
+  return (
+    <div className="watchlater-container">
+      <h1>Watch Later</h1>
+      <div className="video-container">
+        {watchLaterList.length === 0 && <h2>No videos added in watchlist yet!</h2>}
+        {watchLaterList.map((video) => (
+          <VideoCard video={video} key={video._id} />
+        ))}
+      </div>
+    </div>
+  );
+};
