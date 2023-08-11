72cebbee913b671ef13e5fa88dbadc06e834c3f9
arpit gupta <32380690+arpit0498@users.noreply.github.com>
2023-08-11 21:25:30 +0530

    Update watchLater.jsx

--- src/pages/watchLater/watchLater.jsx
+++ src/pages/watchLater/watchLater.jsx
@@ -1,22 +1,22 @@
-import { useDataContext } from "../../context/data/DataContext";
+// import { useDataContext } from "../../context/data/DataContext";
 
-import "./watchlater.layout.css";
-import { VideoCard } from "../../component/VideoCard/VideoCard";
+// import "./watchlater.layout.css";
+// import { VideoCard } from "../../component/VideoCard/VideoCard";
 
-export const WatchLater = () => {
-  const { watchLaterList } = useDataContext();
-  return (
-    <div className="watchlater-container">
-      <h1>Watch Later</h1>
-      <div className="video-container">
-        {watchLaterList.length === 0 && <h2>No videos added in watchlist yet!</h2>}
-        {watchLaterList.map((video) => (
-          <VideoCard video={video} key={video._id} />
-        ))}
-      </div>
-    </div>
-  );
-};
+// export const WatchLater = () => {
+//   const { watchLaterList } = useDataContext();
+//   return (
+//     <div className="watchlater-container">
+//       <h1>Watch Later</h1>
+//       <div className="video-container">
+//         {watchLaterList.length === 0 && <h2>No videos added in watchlist yet!</h2>}
+//         {watchLaterList.map((video) => (
+//           <VideoCard video={video} key={video._id} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 // import React from 'react'
 
 // const WatchLater = () => {
